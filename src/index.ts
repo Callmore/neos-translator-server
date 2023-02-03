bf989301ea8e3a86281dd8819e3248f2c8006c99
Callmore <22885888+Callmore@users.noreply.github.com>
2023-02-03 14:48:27 +0000

    Reorder file and implement new checks

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,177 @@ import { Translate } from "@google-cloud/translate/build/src/v2/index.js";
 import { WSPacket } from "./websocketPackets.js";
 import { EventEmitter } from "events";
 
+const validLanguages = {
+    en: true,
+    ja: true,
+    ko: true,
+    ru: true,
+    zh: true,
+    fr: true,
+};
+function isValidLanguage(lang: string) {
+    return Object.hasOwn(validLanguages, lang);
+}
+
+const escapedCharacters = {
+    ".": true,
+    "*": true,
+    "+": true,
+    "?": true,
+    "^": true,
+    $: true,
+    "{": true,
+    "}": true,
+    "(": true,
+    ")": true,
+    "|": true,
+    "[": true,
+    "]": true,
+    "\\": true,
+};
+
+function neosEscape(text: string) {
+    const output: string[] = [];
+    for (const codepoint of text) {
+        if (Object.hasOwn(escapedCharacters, codepoint)) {
+            output.push(`\\${codepoint}`);
+            continue;
+        }
+        // Try matching a special character
+        let character = undefined;
+        switch (codepoint) {
+            case "\t":
+                character = "t";
+                break;
+            case "\n":
+                character = "n";
+                break;
+            case "\f":
+                character = "f";
+                break;
+            case "\r":
+                character = "r";
+                break;
+        }
+
+        if (character != undefined) {
+            output.push(`\\${character}`);
+            continue;
+        }
+
+        output.push(codepoint);
+    }
+    return output.join("");
+}
+
+// const translate = new Translate();
+
+async function translateText(text: string, langFrom: string, langTo: string) {
+    // const result = await translate.translate(text, {
+    //     from: langFrom,
+    //     to: langTo,
+    // });
+
+    // return result[0];
+    return new Promise<string>((resolve) => resolve(text.toUpperCase()));
+}
+
+function initSpeechRecognitionClient(
+    ws: WebSocket,
+    url: URL,
+    targetUser: string,
+    connectionEvents: EventEmitter
+) {
+    let languageFrom = url.searchParams.get("langfrom")!;
+    if (languageFrom == undefined || !isValidLanguage(languageFrom)) {
+        ws.close(1002, "langfrom query parameter missing or invalid.");
+        return;
+    }
+
+    let languageTo = url.searchParams.get("langto")!;
+    if (languageTo == undefined || !isValidLanguage(languageTo)) {
+        ws.close(1002, "langto query parameter missing or undefined.");
+        return;
+    }
+
+    if (languageFrom == languageTo) {
+        ws.close(1002, "langfrom and langto are the same.");
+        return;
+    }
+
+    ws.on("message", async (message) => {
+        try {
+            const data = JSON.parse(message.toString()) as WSPacket;
+            ({ languageFrom, languageTo } = await parseIncommingPacket(
+                data,
+                targetUser,
+                languageFrom,
+                languageTo,
+                ws,
+                connectionEvents
+            ));
+        } catch (e) {
+            ws.close(1002, "Invalid request.");
+        }
+    });
+}
+
+async function parseIncommingPacket(
+    data: WSPacket,
+    targetUser: string | null,
+    languageFrom: string,
+    languageTo: string,
+    ws: WebSocket,
+    connectionEvents: EventEmitter
+) {
+    switch (data.type) {
+        case "partialRecognition":
+            if (connectionEvents.listenerCount(`partial-${targetUser}`) <= 0) {
+                break;
+            }
+
+            connectionEvents.emit(`partial-${targetUser}`, data.text);
+            break;
+
+        case "finalRecognition":
+            if (connectionEvents.listenerCount(`final-${targetUser}`) <= 0) {
+                break;
+            }
+
+            const translated = await translateText(
+                data.text,
+                languageFrom,
+                languageTo
+            );
+
+            connectionEvents.emit(`final-${targetUser}`, data.text, translated);
+            break;
+
+        case "changeLanguage":
+            // Check that the data is valid
+            if (
+                !isValidLanguage(data.langFrom) ||
+                !isValidLanguage(data.langTo)
+            ) {
+                ws.close(1002, "Invalid language.");
+                break;
+            }
+
+            // It must be valid... right?
+            languageFrom = data.langFrom;
+            languageTo = data.langTo;
+
+        default:
+            ws.close(1002, "Invalid packet type.");
+    }
+    return { languageFrom, languageTo };
+}
+
 export function createWebSocketServer(
     options?: ServerOptions | undefined,
     callback?: (() => void) | undefined
 ) {
     const wss = new WebSocketServer(options, callback);
-
-    // const translate = new Translate();
-
-    const validLanguages = {
-        en: true,
-        ja: true,
-        ko: true,
-        ru: true,
-        zh: true,
-        fr: true,
-    };
-
-    function isValidLanguage(lang: string) {
-        return Object.hasOwn(validLanguages, lang);
-    }
 
     const neosConnectionEvents = new EventEmitter();
 
@@ -51,16 +202,21 @@ export function createWebSocketServer(
             return;
         }
 
-        const endpoint = url.pathname.split("/", 1);
-        if (endpoint.length != 2) {
+        const endpoint = url.pathname.split("/");
+        if (endpoint.length <= 2) {
             ws.close(1002, "Invalid pathname");
             return;
         }
 
-        switch (endpoint[1]) {
+        switch ("/" + endpoint.slice(2).join("/")) {
             case "/speech":
                 // This is the speech recognition client connecting to send text to translate.
-                initSpeechRecognitionClient(ws, url, targetUser);
+                initSpeechRecognitionClient(
+                    ws,
+                    url,
+                    targetUser,
+                    neosConnectionEvents
+                );
                 break;
 
             case "/neos":
@@ -74,122 +230,6 @@ export function createWebSocketServer(
                 break;
         }
     });
-
-    const escapedCharacters = {
-        ".": true,
-        "*": true,
-        "+": true,
-        "?": true,
-        "^": true,
-        $: true,
-        "{": true,
-        "}": true,
-        "(": true,
-        ")": true,
-        "|": true,
-        "[": true,
-        "]": true,
-        "\\": true,
-    };
-
-    function initSpeechRecognitionClient(
-        ws: WebSocket,
-        url: URL,
-        targetUser: string
-    ) {
-        let languageFrom = url.searchParams.get("langfrom")!;
-        if (languageFrom == undefined || !isValidLanguage(languageFrom)) {
-            ws.close(1002, "langfrom query parameter missing or invalid.");
-            return;
-        }
-
-        let languageTo = url.searchParams.get("langto")!;
-        if (languageTo == undefined || !isValidLanguage(languageTo)) {
-            ws.close(1002, "langto query parameter missing or undefined.");
-            return;
-        }
-
-        if (languageFrom == languageTo) {
-            ws.close(1002, "langfrom and langto are the same.");
-            return;
-        }
-
-        ws.on("message", async (message) => {
-            try {
-                const data = JSON.parse(message.toString()) as WSPacket;
-                ({ languageFrom, languageTo } = await parseIncommingPacket(
-                    data,
-                    targetUser,
-                    languageFrom,
-                    languageTo,
-                    ws
-                ));
-            } catch (e) {
-                ws.close(1002, "Invalid request.");
-            }
-        });
-    }
-
-    async function parseIncommingPacket(
-        data: WSPacket,
-        targetUser: string | null,
-        languageFrom: string,
-        languageTo: string,
-        ws: WebSocket
-    ) {
-        switch (data.type) {
-            case "partialRecognition":
-                if (
-                    neosConnectionEvents.listenerCount(
-                        `partial-${targetUser}`
-                    ) <= 0
-                ) {
-                    break;
-                }
-
-                neosConnectionEvents.emit(`partial-${targetUser}`, data.text);
-                break;
-
-            case "finalRecognition":
-                if (
-                    neosConnectionEvents.listenerCount(`final-${targetUser}`) <=
-                    0
-                ) {
-                    break;
-                }
-
-                const translated = await translateText(
-                    data.text,
-                    languageFrom,
-                    languageTo
-                );
-
-                neosConnectionEvents.emit(
-                    `final-${targetUser}`,
-                    data.text,
-                    translated
-                );
-                break;
-
-            case "changeLanguage":
-                // Check that the data is valid
-                if (
-                    !isValidLanguage(data.langFrom) ||
-                    !isValidLanguage(data.langTo)
-                ) {
-                    ws.close(1002, "Invalid language.");
-                    break;
-                }
-
-                // It must be valid... right?
-                languageFrom = data.langFrom;
-                languageTo = data.langTo;
-
-            default:
-                ws.close(1002, "Invalid packet type.");
-        }
-        return { languageFrom, languageTo };
-    }
 
     function initNeosListener(ws: WebSocket, targetUser: string | null) {
         const partialSend = (text: string) => {
@@ -214,53 +254,5 @@ export function createWebSocketServer(
                 finalSend
             );
         });
-    }
-
-    function neosEscape(text: string) {
-        const output: string[] = [];
-        for (const codepoint of text) {
-            if (Object.hasOwn(escapedCharacters, codepoint)) {
-                output.push(`\\${codepoint}`);
-                continue;
-            }
-            // Try matching a special character
-            let character = undefined;
-            switch (codepoint) {
-                case "\t":
-                    character = "t";
-                    break;
-                case "\n":
-                    character = "n";
-                    break;
-                case "\f":
-                    character = "f";
-                    break;
-                case "\r":
-                    character = "r";
-                    break;
-            }
-
-            if (character != undefined) {
-                output.push(`\\${character}`);
-                continue;
-            }
-
-            output.push(codepoint);
-        }
-        return output.join("");
-    }
-
-    async function translateText(
-        text: string,
-        langFrom: string,
-        langTo: string
-    ) {
-        // const result = await translate.translate(text, {
-        //     from: langFrom,
-        //     to: langTo,
-        // });
-
-        // return result[0];
-        return new Promise<string>((resolve) => resolve(text.toUpperCase()));
     }
 }
